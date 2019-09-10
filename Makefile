b3f0947586fa630ebb9e68d7a61056ea79a820eb
Aarni Koskela <akx@iki.fi>
2019-09-10 18:16:40 +0300

    Add category_name metadata

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 doc.tsv: items.jsonl
-	jq -r -s 'sort_by(.name, .sku) | .[] | [.sku,.name,.["package-info"],.url] | @tsv' items.jsonl | uniq > doc.tsv
+	jq -r -s 'sort_by(.category_name, .name, .sku) | .[] | [.category_name,.sku,.name,.["package-info"],.url] | @tsv' items.jsonl | uniq > doc.tsv
 
 items.jsonl: scrape.py
 	scrapy runspider scrape.py -o items.jsonl -t jsonlines
--- scrape.py
+++ scrape.py
@@ -6,11 +6,16 @@ categories = ["soft-drinks", "waters", "juices", "special-drinks"]
 
 class Jider(scrapy.Spider):
     name = "blogspider"
-    start_urls = [
-        f"https://www.juomamaailma.fi/fi/tuotteet/{cat}?p=1" for cat in categories
-    ]
 
-    def parse(self, response):
+    def start_requests(self):
+        for category_name in categories:
+            yield scrapy.Request(
+                url=f"https://www.juomamaailma.fi/fi/tuotteet/{category_name}?p=1",
+                cb_kwargs={"category_name": category_name},
+                callback=self.parse_category_page,
+            )
+
+    def parse_category_page(self, response, category_name):
 
         for product in response.css(".products.wrapper .product-item-info"):
             yield {
@@ -20,7 +25,12 @@ class Jider(scrapy.Spider):
                 .get()
                 .strip(),
                 "url": product.css(".product-item-link::attr(href)").get().strip(),
+                "category_name": category_name,
             }
 
         for page_link in response.css("div.pages li.item a"):
-            yield response.follow(page_link, self.parse)
+            yield response.follow(
+                page_link,
+                self.parse_category_page,
+                cb_kwargs={"category_name": category_name},
+            )
